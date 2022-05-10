2473390ef05ccf975bbb7f24d98da31d9e1a8d62
packettoobig <53047591+packettoobig@users.noreply.github.com>
2022-05-10 19:48:44 +0200

    Newer GeoIP DB

--- root/dockerinstall/install.sh
+++ root/dockerinstall/install.sh
@@ -5,10 +5,25 @@
 apt-get -qq update && \
 apt-get -qq install \
     ca-certificates \
-    deluged=1.3.15-2
+    curl \
+    deluge-console=1.3.15-2 \
+    deluged=1.3.15-2 \
+    python-crypto \
+    python-libtorrent \
+    python3-crypto \
+    python3-libtorrent \
+    xfsprogs
+
+# Update GeoIP
+curl -o /usr/share/GeoIP/GeoIP.dat \
+    -L "https://ipfs.infura.io/ipfs/QmWTWcPRRbADZcLcJeANZmcJZNrcpmuQgKYBi6hGdddtC6"
 
 # Cleanup
-apt-get -qq purge lib-*dev build-essential
+apt-get -qq purge \
+    curl \
+    lib-*dev \
+    build-essential
+
 apt-get -qq purge --autoremove
 apt-get -qq clean
 rm -rf /var/lib/apt/lists/* \
